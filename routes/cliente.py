3123a4e9d751552318ed2a8b921866952a532279
evandro-batista <evan_batista@hotmai.com>
2024-08-12 20:49:58 -0400

    update-2

--- routes/cliente.py
+++ routes/cliente.py
@@ -45,8 +45,22 @@ def form_edit_cliente(cliente_id):
 
 @cliente_route.route('/<int:cliente_id>/update', methods=['PUT'])
 def atualizar_cliente(cliente_id):
+    cliente_editado = None
     """Atualizar informações do cliente"""
-    pass
+    # obter dados do formulario de edição
+    data = request.json
+
+    #obter usuario pelo ik
+    for c in CLIENTES:
+        if c['id'] == cliente_id:
+            c['nome'] = data['nome']
+            c['email'] = data['email']
+
+            cliente_editado = c
+
+    # editar usuario
+    return render_template('item_cliente.html', cliente=cliente_editado)
+    
 
 @cliente_route.route('/<int:cliente_id>/edit', methods=['DELETE'])
 def deletar_cliente(cliente_id):
